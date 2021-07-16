7ec0f4fd2734aa05716692200753c98592417c5c
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-07-17 00:29:56 +0100

    latest commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import logo from './logo.svg';
+
 function App() {
+  const json=[
+    {firstname: 'tye',lastname:'robo',age:2, inRepair:false},
+    {firstname: 'rye',lastname:'toy',age:1, inRepair:true},
+    {firstname: 'pye',lastname:'toaster',age:4, inRepair:true},
+]
+
+let cssClasses=''
+
+const options = {}
+const [state, setState] = React.useState({})
+const idColIdx = options.idCol ? Object.keys(state.json[0]).indexOf(options.idCol) : 0
+const handleRowClick=(e) =>{
+  if (!e.currentTarget.classList.contains('selected')) {
+    let selectedRow = {}
+    state.json.forEach(row => {
+        const rowId = row[Object.keys(row)[idColIdx]]
+        if (rowId.toString() === e.currentTarget.id.toString())
+            selectedRow = row
+    });
+}
+else {
+
+}
+
+
+}
+
+const createRows = () =>{
+  return json.map((row, index) => {
+   // const rowId = row[Object.keys(row)[idColIdx]] // eslint-disable-next-line
+    if (state.selectedRow === row ) {
+        return <tr key={index} className={state.selectedRow === row ? `${cssClasses}` : ""} id={rowId} onClick={handleRowClick} >
+            <Cells row={row}  />
+        </tr>
+    } else {
+        return <tr key={index} className={state.selectedRow === row ? `${cssClasses}` : ""} id={rowId} onClick={handleRowClick} >
+            <Cells  row={row}  />
+        </tr>
+    }
+
+})
+}
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Test Page
       </header>
+
+      <div className="main">
+<table>
+  <thead>
+    <tr>
+      <th>FirstName</th> <th>LastName</th ><th>age</th><th>inRepair</th>
+    </tr>
+  </thead>
+  <tbody>
+    {createRows()}
+  </tbody>
+</table>
+
+
+      </div>
     </div>
   );
 }
