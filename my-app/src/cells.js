417c9366ec9a22fc38bc085a7d8047174a3c9ba8
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-07-17 02:44:21 +0100

    cells

--- my-app/src/cells.js
+++ my-app/src/cells.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const Cells =({row})=>{
-return <td>123</td>
+const Cells =({selectedRow,row,textInputChange,checkInputChange })=>{
+    
+    
+    var cols=Object.keys(row)
+
+
+    const inputChange = (e)  =>{
+    const name= e.currentTarget.name.toString() 
+    const value=e.currentTarget.value
+    textInputChange(name,value)
+
+    }
+   
+    return cols.map((key)=>{
+      
+     if(typeof row[key]==="boolean"){
+        return <td key={key} >
+            <input name={key} type="checkbox" checked={row[key]}></input>
+          </td>
+     }else{
+
+     }
+     return <td key={key}><input  name={key} onChange={inputChange} value={row[key].toString()}></input> </td>
+
+    })
+
+
+
 }
 
 export default Cells
